f2580304503c7718654c1fdce5083e4f00ddade8
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-02-22 21:56:28 -0500

    Done.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,12 @@
-// your ImageSlider code here!
+import React, { Component } from "react";
+
+export default class ImageSlider extends Component {
+  constructor() {
+    super();
+    this.state = { currentSlideIndex: 0 };
+  }
+
+  render() {
+    return <span>I am on slide {this.state.currentSlideIndex}</span>;
+  }
+}
